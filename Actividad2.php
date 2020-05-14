79d5f74f95416b414dae380352a7ef15867d7af5
NataliaPerez08 <65318136+NataliaPerez08@users.noreply.github.com>
2020-05-14 13:11:27 -0500

    FIX:Corregi errores de ortografia

--- Actividad2.php
+++ Actividad2.php
@@ -1,6 +1,6 @@
 <?php
-echo "Mi nombre es Pérez Romero Natalia Abigail";
-echo "<p>He aprendido más de php en esta semana que en mis dos años de ETE :)</p>
-<p>Y aprendí a crear repositorios en Git</p>
-<p>Me ha gustado el estres de nunca acabar todas mis practicas :D";
+echo "Mi nombre es Perez Romero Natalia Abigail";
+echo "<p>He aprendido mÃ¡s de php en esta semana que en la ETE :)</p>
+<p>Y aprendi a crear repositorios en Git</p>
+<p>Me ha gustado el estres de nunca acabar todas mis practicas :D"</p>
 ?>
