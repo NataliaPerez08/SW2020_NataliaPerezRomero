43f4f754a819f1b0b8df616ff1dfa70983ad5e19
natalia_perez08 <natalia_perez08@comunidad.unam.mx>
2020-05-14 13:06:02 -0500

    FEAT: Se creo la actividad 2

---
+++ Actividad2.php
@@ -0,0 +1,6 @@
+<?php
+echo "Mi nombre es Pérez Romero Natalia Abigail";
+echo "<p>He aprendido más de php en esta semana que en mis dos años de ETE :)</p>
+<p>Y aprendí a crear repositorios en Git</p>
+<p>Me ha gustado el estres de nunca acabar todas mis practicas :D";
+?>
